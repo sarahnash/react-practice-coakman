80fa275abf7f00b65edfd741c5a615c5d1894b96
sarahnash <sarahnash1112@gmail.com>
2019-02-21 19:41:47 -0600

    working tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import HelloReact from './HelloReact.js'
+
+import Tabs from './Tabs'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Making some tabs</h1>
+      <Tabs>
+        <div label="Hello React">
+        <HelloReact/>
+        </div>
+        <div label="Lorem Ipsum">
+        Lorem Ipsum component will go here
+        </div>
+        <div label="Login Form">
+        Login Form component will go here
+        </div>
+      </Tabs>
+        
       </div>
     );
   }
---
+++ src/HelloReact.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class HelloReact extends React.Component {
+  constructor(props) {
+    super(props);
+   // don't really need this yet
+  }
+
+  render() {
+    return (
+      <div className="HelloReact">
+        <h1>Hello React!</h1>
+        <div>
+          React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+        </div>
+        <br/>
+        <div>
+          Declarative views make your code more predictable and easier to debug.
+        </div>
+      </div>
+    ) 
+  }
+  
+}
+
+export default HelloReact
